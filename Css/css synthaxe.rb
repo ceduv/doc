cef065659e92487ba405e25fca3d1dd659c968ab
ced duv <cedduvillier@gmail.com>
2021-06-26 19:25:05 +0200

    Fin css code_academy

---
+++ Css/css synthaxe.rb
@@ -0,0 +1,18 @@
+#           CSS 
+
+#------------------------------
+#       SYNTHAXE    
+
+#--- css
+p{
+    color:blue;
+}
+#--- html
+# en ligne 
+<p style="coor:blue;">toto</p>
+# ds le head
+<style>
+p{
+  color : green;
+}
+</style>
