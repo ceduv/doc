d56459e43819d9e2a8aa262e42b88dca778f042b
cedric duvillier <cedduvillier@gmail.com>
2021-07-10 01:20:08 +0200

    oubli

--- git/git flow.rb
+++ git/git flow.rb
@@ -6,3 +6,12 @@
 
 # git-flow est un modèle de branche,
 # et un plugin git pour add cmd
+
+vous ajouterez des features pour les versions majeures et mineures uniquement,
+vous créerez des release pour des versions majeures ou mineures,
+vous créerez des hotfix pour les versions patch.
+
+(master) $ git flow init
+
+git log --graph --oneline
+# voir les branches
