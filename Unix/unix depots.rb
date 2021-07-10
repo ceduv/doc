a850352deb9e31d7350e6a06d016d89a23ae01fd
cedric duvillier <cedduvillier@gmail.com>
2021-07-10 02:00:52 +0200

    blablabla

---
+++ Unix/unix depots.rb
@@ -0,0 +1,18 @@
+#---------------------------
+#          UNIX
+ 
+
+#----------DEPOTS--------------
+
+depot francais par defaut
+http://fr.archive.ubuntu.com/ubuntu/
+
+
+#--------------VOC--------------
+
+paquet      # c'est un programme « prêt à l'emploi »,
+
+dépendance  # un paquet peut avoir besoin de plusieurs autres
+            # pour fonctionner, il a des dépendances
+
+dépôt       # serveur sur lequel on va télécharger nos paquets.
---
+++ Unix/unix man.rb
@@ -0,0 +1,23 @@
+#---------------------------
+#          UNIX
+ 
+
+#-----------MAN--------------
+
+man mkdir                       # doc de la commande mkdir
+
+
+#--------DEPLACEMENTS---------
+
+Q                               # quitter le manuel
+fleche                          # ligne par ligne
+PageUp / PageDown / Espace      # de page en page.
+Home                            # début
+Fin                             # fin
+
+ / (slash)                      # pour effectuer une recherche
+                                # Tapez ensuite le mot que vous recherchez
+                                # Entrée.
+                                # automatiquement placés sur le premier résultat
+                                # résultat suivant, tapez à nouveau / puis Entrée
+
